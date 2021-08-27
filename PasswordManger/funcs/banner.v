f4471ea77b8cfdbbb017cbcdb3c13790c87f5e09
WhosGotFrost <71531086+WhosGotFrost@users.noreply.github.com>
2021-08-27 12:03:52 -0400

    Create banner.v

---
+++ PasswordManger/funcs/banner.v
@@ -0,0 +1,14 @@
+module funcs
+
+pub fn banner(){
+	println("
+╔════════╦══════════════════════════════════╗
+║ Menu # ║ Password Manager by WhosGotFrost ║
+╠════════╬══════════════════════════════════╣
+║ 1.     ║ Add Password                     ║
+║ 2.     ║ Your Saved Passwords             ║
+║ 3.     ║ Clears Password txt file         ║
+║ exit   ║ will exit the tool               ║
+╚════════╩══════════════════════════════════╝
+	")
+}
